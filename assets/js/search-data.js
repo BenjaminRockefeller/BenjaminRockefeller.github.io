bc1c45a99e5558d0f1c3186cdb9251df2ae719c4
BenjaminRockefeller <jianming001@e.ntu.edu.sg>
2025-06-21 10:48:33 +0000

    Deploying to gh-pages from @ BenjaminRockefeller/benjaminrockefeller.github.io@7cbcf1d1b61c32c22130fed6aa6fb7d4d38e3ba6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -423,17 +423,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{id: "projects-project-1-financial-research-report-generation-platform",
+          section: "News",},{id: "projects-project-1-financial-research-report-generation-platform",
           title: 'Project 1｜Financial Research Report Generation Platform',
           description: "",
           section: "Projects",handler: () => {
